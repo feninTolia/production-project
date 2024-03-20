51996b5083590febd434b1fa51dd85fbc0e186dc
Anatolii Fenin <fenintolja@gmail.com>
2024-03-20 14:04:56 +0200

    ref: test render component method

--- src/shared/lib/tests/renderComponent/renderComponent.tsx
+++ src/shared/lib/tests/renderComponent/renderComponent.tsx
@@ -4,10 +4,18 @@ import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
 import i18n from 'shared/config/i18n/i18nForTesting';
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const renderComponent = (component: ReactNode) => {
+export interface IRenderComponentOptions {
+  route?: string;
+}
+
+export const renderComponent = (
+  component: ReactNode,
+  options: IRenderComponentOptions = {}
+) => {
+  const { route = '/' } = options;
+
   return render(
-    <MemoryRouter>
+    <MemoryRouter initialEntries={[route]}>
       <I18nextProvider i18n={i18n}>{component}</I18nextProvider>
     </MemoryRouter>
   );

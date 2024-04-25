b0a50482e5e6dacc24aa14025ef0e515122f4534
Anatolii Fenin <fenintolja@gmail.com>
2024-04-25 10:43:16 +0300

    add fetching articles, view type selector

--- src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
+++ src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
@@ -2,10 +2,10 @@ import { IStateSchema } from 'app/providers/StoreProvider';
 import { IArticlesView } from 'entities/Article';
 
 export const getArticlesPageIsLoading = (state: IStateSchema) =>
-  state.articlesPage?.isLoading || false;
+  state.articlesPage?.isLoading ?? false;
 
 export const getArticlesPageError = (state: IStateSchema) =>
   state.articlesPage?.error;
 
 export const getArticlesPageView = (state: IStateSchema) =>
-  state.articlesPage?.view || IArticlesView.SMALL;
+  state.articlesPage?.view ?? IArticlesView.SMALL;

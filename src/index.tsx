11a800dd660433780a8985eb947f00f5b00ea155
Anatolii Fenin <fenintolja@gmail.com>
2024-05-15 14:48:40 +0300

    migrate to react 18, add mock api for storybook

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
-// eslint-disable-next-line react/no-deprecated
-import { render } from 'react-dom';
 import { App } from 'app/App';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'app/providers/ThemeProvider';
-import { Suspense } from 'react';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { StoreProvider } from 'app/providers/StoreProvider';
-import 'shared/config/i18n/i18n';
+import { ThemeProvider } from 'app/providers/ThemeProvider';
 import 'app/styles/index.scss';
+import { Suspense } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import 'shared/config/i18n/i18n';
 
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
